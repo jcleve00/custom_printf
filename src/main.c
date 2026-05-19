b81fc39c3f0312c954ad8c396349eea963aed776
Grover2x <jcleve00@icloud.com>
2026-05-19 20:56:49 +0000

    Fixed errors with menu

--- src/main.c
+++ src/main.c
@@ -18,9 +18,9 @@ int main(){
         customPrintf("1. Test customPrintf w/ char.\n");
         customPrintf("2. Test customPrintf w/ string.\n");
         customPrintf("3. Test customPrintf w/ int.\n");
-        customPrintf("4. Test customPrintf w/ float\n.");
-        customPrintf("5. Test customPrintf w/ binary\n.");
-        customPrintf("6. Test customPrintf w/ multiple specifiers\n.");
+        customPrintf("4. Test customPrintf w/ float.\n");
+        customPrintf("5. Test customPrintf w/ binary.\n");
+        customPrintf("6. Test customPrintf w/ multiple specifiers.\n");
         customPrintf("7. Quit.\n");
 
         customPrintf("Choose from the above options: ");
@@ -28,7 +28,7 @@ int main(){
 
         // If menu choice is valid call the function at that index in menuFunctions
         if (choice >= 1 && choice <= 6){
-            menuFunctions[choice - 1];
+            menuFunctions[choice - 1]();
         }
         else {
             customPrintf("Invalid option.\n");
