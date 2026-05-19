944e618e710f49cea8d9d6dc5ecc3156ad5e425b
Wynoami Glasser <wynoami@gmail.com>
2026-05-19 17:08:52 +0000

    Added printInt and specifier dictionary

--- src/custom_printf.h
+++ src/custom_printf.h
@@ -7,7 +7,26 @@
 #include <unistd.h>
 #include <string.h>
 
+// Struct definition
+typedef struct format_specifier {
+	char specifier;
+	int (*function)(va_list);
+} FormatSpec;
+
+	FormatSpec specs[] = {
+		{'c', printChar},
+		{'s', printString},
+		{'d', printInt},
+		{'i', printInt},
+		{'b', printBinary},
+		{'\0', NULL}
+	};
+    
+// Function Prototypes
 int customPrintf(const char *string, ...);
-void printChar(char c);
-void printInt(int d);
+int printChar(va_list args);
+int printString(va_list args);
+int printInt(va_list args);
+int printBinary(va_list args);
+
 #endif
