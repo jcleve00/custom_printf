0c924615b17ad36920ac79831db678c2f5a05ef3
Wynoami Glasser <wynoami@gmail.com>
2026-05-20 17:21:21 +0000

    Fixed comments from // to /**/

--- src/print_binary.c
+++ src/print_binary.c
@@ -2,22 +2,22 @@
 
 int printBinary(va_list args){
     int n = va_arg(args, int);
-    int binaryNum[32]; // Array to store binary digits assuming 32 bits
+    int binaryNum[32]; /* Array to store binary digits assuming 32 bits */
     int i = 0;
     int charsPrinted = 0;
 
-    char *buffer = (char *)malloc(32 * sizeof(char)); // Dynamically allocate memory to array
+    char *buffer = (char *)malloc(32 * sizeof(char)); /* Dynamically allocate memory to array */
     if (buffer == NULL){
         return -1;
     }
 
-    // if number is 0 no conversion necessary
+    /* if number is 0 no conversion necessary */
     if (n == 0){
         write(1, "0", 1);
         free(buffer);
         return 1;
     }
-    // Convert integer to binary and store in binaryNum
+    /* Convert integer to binary and store in binaryNum */
     while (n > 0){
         binaryNum[i] = n % 2;
         n = n / 2;
@@ -25,7 +25,7 @@ int printBinary(va_list args){
     }
     int digitCount = i;
 
-    // Loop through binaryNum, converting digits to chars and store in buffer in reverse order
+    /* Loop through binaryNum, converting digits to chars and store in buffer in reverse order */
     int bufferIndex = 0;
 
     for (int j = digitCount - 1; j >= 0; j--){
@@ -33,7 +33,7 @@ int printBinary(va_list args){
         bufferIndex++;
     }
     
-    // Get length of buffer. Print and count digits
+    /* Get length of buffer. Print and count digits */
     for (i = 0; i < bufferIndex;i++){
         write(1, &buffer[i], 1);
         charsPrinted++;
--- src/print_int.c
+++ src/print_int.c
@@ -3,9 +3,9 @@
 int printInt(va_list args) {
 	int n = va_arg(args, int);
 	int charsPrinted = 0;
-	unsigned int num; // use unsigned to safely handle conversion to positive
+	unsigned int num; /* use unsigned to safely handle conversion to positive */
 	
-	// Edge case 0
+	/*Edge case 0 */
 	if (n < 0) {
 		charsPrinted += write(1, "-", 1);
 		// Safely make the number positive
@@ -14,7 +14,7 @@ int printInt(va_list args) {
 			num = (unsigned int)n;
 	}
 	
-	// Dynamic memory: allocate an array to hold our digits
+	/* Dynamic memory: allocate an array to hold our digits*/
 	char *buffer = (char *)malloc(12 * sizeof(char));
 	if (buffer == NULL) {
 		return -1;
@@ -22,14 +22,14 @@ int printInt(va_list args) {
 	
 	int i = 0;
 	while (num > 0) {
-			// Get the last digit by finding the remainder of division by 10
-			// Adding '0' converts the integer digit (e.g., 5) to the ASCII character ('5')
+			/* Get the last digit by finding the remainder of division by 10
+			 Adding '0' converts the integer digit (e.g., 5) to the ASCII character ('5') */
 			buffer[i] = (num % 10) + '0';
 			num = num / 10; // Remove the last digit
 			i++;
 		}
 		
-		// The digits were extracted backwards, so we print them in reverse
+		/* The digits were extracted backwards, so we print them in reverse */
 		for (int j = i - 1; j >= 0; j--) {
 				charsPrinted += write(1, &buffer[j], 1);
 		}
